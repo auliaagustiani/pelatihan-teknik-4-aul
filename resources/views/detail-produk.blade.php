696a77dc77fae63009bda157772e0a984867c1a4
Aulia Agustiani <agustianiaulia61@gmail.com>
2026-05-16 23:02:09 +0700

    Misi 2: Selesai membuat detail produk blade

--- resources/views/detail-produk.blade.php
+++ resources/views/detail-produk.blade.php
@@ -2,37 +2,42 @@
 
 @section('detail_produk')
 <body style="background-color: #F3EAE3;">
-<div class="container my-5">
-    <div class="col-md-8">
-    <a href="/" class="btn btn-secondary mb-3">Kembali ke Laptop Si Unyil</a>
+<div class="container py-5">
+    <div class="col-md-7">
+    <a href="/" class="btn btn-secondary mb-4">Kembali ke Laptop Si Unyil</a>
                         
-        <div class="col-md-7">
+        <div class="col-md-7"></div>
+            <div class="card-body"></div>
             <img src="{{ asset('img/espresso.jpeg')}}" height="100px">  
             <h2 class="text-success">Espresso</h2>
             <h5 class="text-muted">Rp 15.000</h5>
             <h5>Deskripsi Produk:</h5>
-            <p>Kopi hitam dengan rasa yang kuat dan aroma yang khas, dibuat dari ekstrak biji kopi pilihan. Disajikan dalam ukuran kecil, eitss, walaupun disajikan dalam ukuran kecil, jangan remehkan powernya, power rangers kah? aku yang pink, wekwekwek.</p>
+            <p class="card-text">Kopi hitam dengan rasa yang kuat dan aroma yang khas, dibuat dari ekstrak biji kopi pilihan. Disajikan dalam ukuran kecil, eitss, walaupun disajikan dalam ukuran kecil, jangan remehkan powernya, power rangers kah? aku yang pink, wekwekwek.</p>
 
-        <div class="col-md-7">
+        <button type="button" class="btn btn-success w-100 fw-bold mb-5" onclick="alert('Pesanan berhasil ditambahkan!')">Beli Sekarang</button>
+
+        <div class="col-md-7"></div>
+            <div class="card-body"></div>
             <img src="{{ asset('img/americano.jpeg')}}" height="100px">  
             <h2 class="text-success">Americano</h2>
             <h5 class="text-muted">Rp 20.000</h5>
             <h5>Deskripsi Produk:</h5>
-            <p>Perpaduan espresso dan air panas dengan rasa kopi yang lebih ringan dan nyaman diminum, asal ga delapan shot aja. Teman terbaik buat nongki atau nugas lama di cafe biar berasa orang paling produktif sedunia, xixixi.</p>
+            <p class="card-text">Perpaduan espresso dan air panas dengan rasa kopi yang lebih ringan dan nyaman diminum, asal ga delapan shot aja. Teman terbaik buat nongki atau nugas lama di cafe biar berasa orang paling produktif sedunia, xixixi.</p>
 
-        <div class="col-md-7">
+        <button type="button" class="btn btn-success w-100 fw-bold mb-5" onclick="alert('Pesanan berhasil ditambahkan!')">Beli Sekarang</button>
+
+        <div class="col-md-7"></div>
+            <div class="card-body"></div>
             <img src="{{ asset('img/lattebaru.jpeg')}}"height="100px">  
             <h2 class="text success">Latte</h2>
             <h5 class="text-muted">Rp 25.000</h5>
             <h5>Deskripsi Produk:</h5>
-            <p>Minuman yang dibuat dari campuran espresso dan susu yang lembut nan creamy. Cocok buat yang ingin ngopi tapi masih takut diserang rasa pahit kehidupan, eh,, rasa kopi maksudnya</p>
+            <p class="card-text">Minuman yang dibuat dari campuran espresso dan susu yang lembut nan creamy. Cocok buat yang ingin ngopi tapi masih takut diserang rasa pahit kehidupan, eh,, rasa kopi maksudnya</p>
 
-        <a href="/" type="button" class="btn btn-success">Beli Sekarang</a>
+        <button type="button" class="btn btn-success w-100 fw-bold mb-5" onclick="alert('Pesanan berhasil ditambahkan!')">Beli Sekarang</button>
 
-                </div>
-            </div>
-        </div>
     </div>
 </div>
+
 </body>
 @endsection
